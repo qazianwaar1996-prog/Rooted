1590ed158aae43f0d4fd680575e5b80d342e72ca
qazianwaar1996-prog <qazi.anwaar1996@gmail.com>
2026-08-07 12:57:42 +0500

    Change base URL in Vite config to '/Rooted/'

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/Rooted/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
